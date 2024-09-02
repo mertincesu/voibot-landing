d01f15ff6c3b7322610967504cdca268ae2be31d
Mert Incesu <mert.incesu03@gmail.com>
2024-09-02 13:00:40 +0300

    updated router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './components/Homepage';
 import JoinUs from './components/JoinUs';
 
 function App() {
   return (
-    <Router basename="/voibot-landing">
+    <div className="App">
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/joinus" element={<JoinUs />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
