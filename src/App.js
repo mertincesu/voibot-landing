6da67bf732797e32c7c64d0b94620f8e45406e89
Mert Incesu <mert.incesu03@gmail.com>
2024-09-02 12:23:13 +0300

    added hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './components/Homepage';
 import JoinUs from './components/JoinUs';
@@ -7,7 +7,7 @@ import JoinUs from './components/JoinUs';
 function App() {
   return (
     <div className="App">
-      <Router basename="/voibot-landing">
+      <Router>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/joinus" element={<JoinUs />} />
