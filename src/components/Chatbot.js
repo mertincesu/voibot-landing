fa7348c26f966eceb34abb8004331ac0480bfa1f
Mert Incesu <mert.incesu03@gmail.com>
2024-09-05 08:49:23 +0300

    added heroku backend endpoints

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -19,7 +19,7 @@ const ChatbotInterface = () => {
   };
 
   useEffect(() => {
-    axios.get('/initialize')
+    axios.get('https://voibot-landing-48974dd599d3.herokuapp.com/initialize')
       .then(response => {
         console.log(response.data.message);
         setIsInitialized(true);
@@ -54,7 +54,7 @@ const ChatbotInterface = () => {
     setIsTyping(true);
 
     try {
-      const response = await axios.post('/chat', { query: input });
+      const response = await axios.post('https://voibot-landing-48974dd599d3.herokuapp.com/chat', { query: input });
       const formattedText = formatMessage(response.data.response);
       setMessages(prev => [...prev, { text: formattedText, sender: 'bot', typing: true }]);
     } catch (error) {
