3c41402dfe5fbce82deb199d4992abb6eaf0b89b
Mert Incesu <mert.incesu03@gmail.com>
2024-09-13 18:42:34 +0300

    fixed backend endpoint access error

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -54,7 +54,7 @@ const ChatbotInterface = () => {
     setIsTyping(true);
 
     try {
-      const response = await axios.post('https://voibot-landing-48974dd599d3.herokuapp.com/api/some-endpoint/chat', { query: input });
+      const response = await axios.post('https://voibot-landing-48974dd599d3.herokuapp.com/chat', { query: input });
       const formattedText = formatMessage(response.data.response);
       setMessages(prev => [...prev, { text: formattedText, sender: 'bot', typing: true }]);
     } catch (error) {
